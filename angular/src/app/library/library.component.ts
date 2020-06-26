ad090a456a1d0e624dc4523a14ca8422db1d07c1
zar-ref <48293815+zar-ref@users.noreply.github.com>
2020-06-26 08:49:10 +0100

    Update library.component.ts

--- angular/src/app/library/library.component.ts
+++ angular/src/app/library/library.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LibraryService } from '../admin-zone/library/library.service';
+import { BookData } from '../admin-zone/library/book-data.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-library',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
-  constructor() { }
+
+  books: BookData[] = []
+  isLoading = false;
+  private booksSub : Subscription;
+
+  constructor(private libraryService: LibraryService) { }
 
   ngOnInit() {
+    this.isLoading = true;
+    this.libraryService.getBooks()
+    this.booksSub = this.libraryService.books.subscribe( books => {
+      if(books){
+        console.log(books);
+        this.books = books
+        this.isLoading = false
+      }else {
+        this.isLoading = false
+      }
+     
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  ngOnDestroy(){
+    if(this.booksSub){
+      this.booksSub.unsubscribe()
+    }
   }
 
 }
